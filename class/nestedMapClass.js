e1947bb95d3292377dba16e5be1dbfb773c60681
Aloïs Christen <alois.christen@divtec.ch>
2018-05-18 11:46:32 +0200

    jsBuilder and new class for Map

---
+++ class/nestedMapClass.js
@@ -0,0 +1,29 @@
+class nestedMap{
+	constructor(level, size ){
+		this.level = level;
+		this.size = size;
+		this.MAXCHILD = Math.pow(size,2);
+		this.childMaps = [];
+	}
+	defineParentMap(parentMap) {
+		this.parentMap = parentMap;	
+	}
+	addChildMap(newChildMap){
+		if(this.childMaps.length < this.MAXCHILD ){
+			newChildMap.defineParentMap(this);
+			this.childMaps[this.childMaps.length-1].setNextMap(newChildMap);
+			this.childMaps.push(newChildMap);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	setNextMap(nextMap){
+		this.nextMap = nextMap;
+	}
+	
+}
+//TODO know the type of your next child
+//TODO learn your new orientation when you add a child
+
---
+++ jsBuilder.vim
@@ -0,0 +1,30 @@
+"delete the file exept first line
+:2,$d
+"append the file and go to last line
+:r %:h\globalConst.js
+:$
+:r %:h\indicators.js
+:$
+:r %:h\graphic.js
+:$
+:r %:h\class\pathClass.js
+:$
+:r %:h\class\planeClass.js
+:$
+:r %:h\class\towerClass.js
+:$
+:r %:h\class\bulletClass.js
+:$
+:r %:h\class\segmentClass.js
+:$
+:r %:h\class\areaTesterClass.js
+:$
+:r %:h\function\fLoadImage.js
+:$
+:r %:h\function\fMainLoop.js
+:$
+:r %:h\function\fCreatePath.js
+:$
+:r %:h\function\fInit.js
+:$
+
